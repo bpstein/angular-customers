458286a9ca6cbafb0095c9c90225e2a99630c609
Ben Stein <bumstain@gmail.com>
2016-06-03 15:28:07 +0100

    Refactored into controller using custom code first, then injecting into Angular module later

--- app/controllers/customersController.js
+++ app/controllers/customersController.js
@@ -17,20 +17,45 @@
 
 // Option 2 => eliminates global variable
 
+// (function() {
+//   angular.module('customersApp')
+//     .controller('CustomersController', function($scope) {
+//       $scope.sortBy = 'name';
+//       $scope.reverse = false;
+//       $scope.customers= [
+//         {joined: '2000-12-05', name:'John', city:'London', orderTotal: 9.5532}, 
+//         {joined: '2005-11-15',name:'Rahul', city:'New York', orderTotal: 10.109}, 
+//         {joined: '2012-04-01',name:'Kumiko', city:'Sydney', orderTotal: 90.5858}, 
+//         {joined: '2007-10-17',name:'Paul', city:'Tokyo', orderTotal: 18.4422}
+//       ]
+//       $scope.doSort = function(propName) {
+//         $scope.sortBy = propName; 
+//         $scope.reverse = !$scope.reverse;
+//       };
+//   });
+// }());
+
+
+// Option 3 => 
+
 (function() {
+
+  var CustomersController = function ($scope) {
+    $scope.sortBy = 'name';
+    $scope.reverse = false;
+    $scope.customers= [
+      {joined: '2000-12-05', name:'John', city:'London', orderTotal: 9.5532}, 
+      {joined: '2005-11-15',name:'Rahul', city:'New York', orderTotal: 10.109}, 
+      {joined: '2012-04-01',name:'Kumiko', city:'Sydney', orderTotal: 90.5858}, 
+      {joined: '2007-10-17',name:'Paul', city:'Tokyo', orderTotal: 18.4422}
+    ]
+    $scope.doSort = function(propName) {
+      $scope.sortBy = propName; 
+      $scope.reverse = !$scope.reverse;
+    };
+  };
+
   angular.module('customersApp')
-    .controller('CustomersController', function($scope) {
-      $scope.sortBy = 'name';
-      $scope.reverse = false;
-      $scope.customers= [
-        {joined: '2000-12-05', name:'John', city:'London', orderTotal: 9.5532}, 
-        {joined: '2005-11-15',name:'Rahul', city:'New York', orderTotal: 10.109}, 
-        {joined: '2012-04-01',name:'Kumiko', city:'Sydney', orderTotal: 90.5858}, 
-        {joined: '2007-10-17',name:'Paul', city:'Tokyo', orderTotal: 18.4422}
-      ]
-      $scope.doSort = function(propName) {
-        $scope.sortBy = propName; 
-        $scope.reverse = !$scope.reverse;
-      };
-  });
+    .controller('CustomersController', CustomersController);
+     
 }());
