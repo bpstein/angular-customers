40d95b0e209b6c9a6791a05bab5872e154c8806a
Ben Stein <bumstain@gmail.com>
2016-06-03 17:28:16 +0100

    Added routes to view customer orders

--- app/controllers/ordersController.js
+++ app/controllers/ordersController.js
@@ -5,6 +5,16 @@
     var customerId = $routeParams.customerId;
     $scope.orders = null;
 
+    function init() {
+      // Search the customers for the customer id 
+      for (var i=0, len=$scope.customers.length; i < len; i++) {
+        if ($scope.customers[i].id === parseInt(customerId)) {
+          $scope.orders = $scope.customers[i].orders;
+          break;
+        }
+      }
+    }
+
     $scope.customers= [
       {
         id: 1, 
@@ -36,7 +46,10 @@
       }, 
       {joined: '2012-04-01',name:'Kumiko', city:'Sydney', orderTotal: 90.5858}, 
       {joined: '2007-10-17',name:'Paul', city:'Tokyo', orderTotal: 18.4422}
-    ]
+    ];
+
+    init();
+
   };
 
   OrdersController.$inject = ['$scope', '$routeParams'];
