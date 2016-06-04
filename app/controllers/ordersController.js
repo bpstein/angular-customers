3bd092d3cc08b90c951f62f3a1b675eeefa7fe7d
Ben Stein <bumstain@gmail.com>
2016-06-04 14:20:10 +0100

    Wired up customer data using Express and JSON calls

--- app/controllers/ordersController.js
+++ app/controllers/ordersController.js
@@ -6,7 +6,14 @@
     $scope.customer = null;
 
     function init() {
-      $scope.customer = customersFactory.getCustomer(customerId);
+      customersFactory.getCustomer(customerId)
+        .success(function(customer) {
+          $scope.customer = customer;
+        })
+        .error(function(data, status, headers, config) {
+          // handle error
+        });
+
     }
 
     init();
